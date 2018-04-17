0e98d343d03a1b13d92b0fbdf26c768a098e9647
Invader Zim <pwstern@gmail.com>
2018-04-16 19:13:58 -0700

    Replaced matched cards after next selection.  Don't indicate selection, if it was one of the matched cards.

--- SetCardGame/SetCardGame.swift
+++ SetCardGame/SetCardGame.swift
@@ -12,7 +12,9 @@ struct SetCardGame
 {
     var numberOfCardsDealt: Int
     var numberOfCardsSelected = 0
-    var isMatchMade: Bool?
+    var isMatchMade: Bool?            // true if 3 selected and match, false if 3 selected and no match, else nil
+    var isPreviousMatchMade = false   // true if 3 selected and match, else false
+    var matchIndices = [Int]()
     var deck = SetCardDeck()
     var cardsDealt = [SetCard]()
     var isCardSelected = [Bool]()
@@ -24,25 +26,35 @@ struct SetCardGame
     
     mutating func cardSelected(at index:Int) {
         if index < isCardSelected.count {                           // make sure index is within array
+            numberOfCardsSelected = isCardSelected.filter { $0 == true }.count
+            if numberOfCardsSelected == 3 {                         // if selected a card while 3 are already selected
+                isCardSelected = isCardSelected.map { _ in false }  // clear all selections
+                if isPreviousMatchMade {
+                    for matchIndex in matchIndices {                // replace matched cards with new cards
+                        if let card = deck.drawRandom() {
+                            cardsDealt[matchIndex] = card
+                        }
+                    }
+                }
+            }
+            if !(isPreviousMatchMade && matchIndices.contains(index)) {
+                isCardSelected[index] = !isCardSelected[index]      // select current card, unless it was part of the previous match
+            }
             isMatchMade = nil
             numberOfCardsSelected = isCardSelected.filter { $0 == true }.count
+            isPreviousMatchMade = false
             if numberOfCardsSelected == 3 {
-                isCardSelected = isCardSelected.map { _ in false }  // set all to false
-                numberOfCardsSelected = 1
-            }
-            isCardSelected[index] = !isCardSelected[index]
-            numberOfCardsSelected = isCardSelected.filter { $0 == true }.count
-            if numberOfCardsSelected == 3 {
-                isMatchMade = checkForMatch()
+                matchIndices = isCardSelected.indices.filter { isCardSelected[$0] == true }
+                let selectedCards = matchIndices.map { cardsDealt[$0] }
+                isMatchMade = checkForMatching(cards: selectedCards)
+                isPreviousMatchMade = isMatchMade!
             }
         }
     }
     
-    func checkForMatch() -> Bool {
-        let indices = isCardSelected.indices.filter { isCardSelected[$0] == true }
-        let selectedCards = indices.map { cardsDealt[$0] }
-        assert(selectedCards.count == 3, "SetCardGame.checkForMatch(\(selectedCards.count)): there should be 3 selected cards, here")
-        if selectedCards[0].rank == selectedCards[1].rank {     // placeholder, for now
+    func checkForMatching(cards: [SetCard]) -> Bool {
+        assert(cards.count == 3, "SetCardGame.checkForMatch(\(cards.count)): there should be 3 selected cards, here")
+        if cards[0].rank == cards[1].rank {     // placeholder, for now
             return true
         }
         return false
